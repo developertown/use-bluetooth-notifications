a2ad47a82eda057719f5c5e9d874b1a919231b1e
Matt Williams <mwilliams@developertown.com>
2020-05-22 10:26:54 -0400

    Updated context types

--- src/useBluetooth.tsx
+++ src/useBluetooth.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { BluetoothConnections } from "./types/Bluetooth";
-import BluetoothContext from "./BluetoothContext";
+import { BluetoothContext } from "./BluetoothContext";
 
 export function useBluetooth() {
-  const context = React.useContext<BluetoothConnections>(BluetoothContext);
+  const context = React.useContext<BluetoothConnections | undefined>(BluetoothContext);
   if (context === undefined) {
     throw new Error("useBluetooth must be used within a BluetoothProvider");
   }
